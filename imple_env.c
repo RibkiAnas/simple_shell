1fb1322fd028a0ef4f24e32c7524658f480f7c2f
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-05-20 03:21:29 +0100

    fix betty and update of hsh

--- imple_env.c
+++ imple_env.c
@@ -5,12 +5,16 @@
 */
 void print_env(void)
 {
-	extern char **environ; /* pointer to the environment variables array */ 
+	extern char **environ;
+
+	/* pointer to the environment variables array */
 	int i; /* index variable */
 
-	for (i = 0; environ[i] != NULL; i++) /* loop through all environment variables */
+	for (i = 0; environ[i] != NULL; i++)
+		/* loop through all environment variables */
 	{
-		write(STDOUT_FILENO, environ[i], strlen(environ[i])); /* write the environment variable to stdout */
+		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+		/* write the environment variable to stdout */
 		write(STDOUT_FILENO, "\n", 1); /* write a new line to stdout */
 	}
 }
